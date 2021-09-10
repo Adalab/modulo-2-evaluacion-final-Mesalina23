5824b0524c38531b4362e1309c0dc8d471417f3d
Mesalina23 <mebrapu23@gmail.com>
2021-09-10 08:49:46 +0200

    Create fetch

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,5 +1,28 @@
 'use strict';
 
-console.log('>> Ready :)');
+const inputSearch = document.querySelector('.js_input');
+const searchButton =  document.querySelectorAll ('.js_searchButton');
+
+let listData= [];
+
+
+function handleSearchButton (){
+  let inputValue = inputSearch.value.toLowerCase();
+
+  fetch (`//api.tvmaze.com/search/shows?q=${inputValue}`)
+    .then ((response)=> response.json())
+    .then ((data)=>{
+      listData = data;
+      paintListData();
+    });
+}
+
+function paintListData(){
+
+}
+
+/*searchButton.addEventListener('click',handleSearchButton);*/
+
+
 
 //# sourceMappingURL=main.js.map
--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
-console.log('>> Ready :)');
+const inputSearch = document.querySelector('.js_input');
+const searchButton =  document.querySelectorAll ('.js_searchButton');
+
+let listData= [];
+
+
+function handleSearchButton (){
+  let inputValue = inputSearch.value.toLowerCase();
+
+  fetch (`//api.tvmaze.com/search/shows?q=${inputValue}`)
+    .then ((response)=> response.json())
+    .then ((data)=>{
+      listData = data;
+      paintListData();
+    });
+}
+
+function paintListData(){
+
+}
+
+/*searchButton.addEventListener('click',handleSearchButton);*/
+
+
